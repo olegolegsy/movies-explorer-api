b889a1f23d1fe3df0e3602edf0b82c78d633534d
Sidorov Oleg <ya.sidorovoleg8700@yandex.ru>
2023-12-18 09:17:08 +0300

    (4) Четвертый коммит: добавлена схема movie.

--- models/movie.js
+++ models/movie.js
@@ -1,5 +1,70 @@
 const mongoose = require('mongoose');
 
-const userMovie = new mongoose.Schema({}, { versionKey: false });
+const userMovie = new mongoose.Schema(
+  {
+    country: { type: String, required: [true, 'Незаполнено поле country'] },
+    director: { type: String, required: [true, 'Незаполнено поле director'] },
+    duration: { type: Number, required: [true, 'Незаполнено поле duration'] },
+    year: { type: String, required: [true, 'Незаполнено поле year'] },
+    description: {
+      type: String,
+      required: [true, 'Незаполнено поле description '],
+    },
+    image: {
+      type: String,
+      required: [true, 'Незаполнено поле image'],
+      validate: {
+        validator(url) {
+          return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(
+            url
+          );
+        },
+        message: 'Введите URL в поле image',
+      },
+    },
+    trailerLink: {
+      type: String,
+      required: [true, 'Незаполнено поле trailerLink'],
+      validate: {
+        validator(url) {
+          return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(
+            url
+          );
+        },
+        message: 'Введите URL в поле trailerLink',
+      },
+    },
+    thumbnail: {
+      type: String,
+      required: [true, 'Незаполнено поле thumbnail'],
+      validate: {
+        validator(url) {
+          return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(
+            url
+          );
+        },
+        message: 'Введите URL в поле thumbnail',
+      },
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    movieId: {
+      type: Number,
+      required: [true, 'Незаполнено поле movieId'],
+    },
+    nameRU: {
+      type: String,
+      required: [true, 'Незаполнено поле nameRU'],
+    },
+    nameEN: {
+      type: String,
+      required: [true, 'Незаполнено поле nameEN'],
+    },
+  },
+  { versionKey: false }
+);
 
 module.exports = mongoose.model('movie', userMovie);
